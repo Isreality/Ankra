e8ae3df6fadc261c5d1e150198139b84cb1b6f3b
Isreality <isrealitycreations@gmail.com>
2025-05-30 11:29:32 +0100

    added new components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import Image from "next/image";
 import Btn from "../components/Btn.jsx";
+// import { Icon } from '@iconify-icon/react';
+import { Icon } from '@iconify/react';
+
 
 
 export default function Home() {
@@ -15,8 +18,42 @@ export default function Home() {
                 </h2><br/>
                 <h1 className="text-primary text-4xl font-bold">70% OFF</h1>
                 <Link href='/shop' className="bg-primary hover:bg-black lg:w-16 py-6 px-16 rounded-md border-fa cursor-pointer text-white text-xs md:text-base font-medium">Shop Now</Link>
-              </div>
-            </div>   
+            </div>
+        </div>  
+
+        {/* Services */} 
+        <div className="flex flex-row justify-between items-center w-full px-4 md:px-8 lg:px-24 py-16">
+          <div className="flex flex-row gap-3">
+            <div>
+              <Icon icon="tabler:truck-delivery" width="65" height="65" className="text-primary font-medium" />
+            </div>
+
+            <div>
+              <h1 className="text-left text-base md:text-lg font-bold">Fast Delivery</h1>
+              <p className="text-left text-sm md:text-base">To Your Doorstep</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-3">
+            <div>
+              <Icon icon="iconamoon:headphone-light" width="60" height="60" className="text-primary font-medium" />
+            </div>
+            <div>
+              <h1 className="text-left text-base md:text-lg font-bold">24/7 Online Support</h1>
+              <p className="text-left text-sm md:text-base">Technical Support</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-3">
+            <div>
+              <Icon icon="bx:wallet" width="60" height="60" className="text-primary font-medium" />
+            </div>
+            <div>
+              <h1 className="text-left text-base md:text-lg font-bold">Secure Payment</h1>
+              <p className="text-left text-sm md:text-base">All Cards Accepted</p>
+            </div>
+          </div>
+        </div>
         
       </main>
     </div>
